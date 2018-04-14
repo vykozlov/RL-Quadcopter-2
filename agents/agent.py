83d60473820e19c61eceaccb3d18292c1120315d
valentin.kozlov <valentin.kozlov@kit.edu>
2018-04-14 04:24:12 +0200

    change gamma = 0.99 back

--- agents/agent.py
+++ agents/agent.py
@@ -187,7 +187,7 @@ class DDPG():
         self.memory = ReplayBuffer(self.buffer_size, self.batch_size)
 
         # Algorithm parameters
-        self.gamma = 0.9  # discount factor
+        self.gamma = 0.99  # discount factor
         self.tau = 0.01  # for soft update of target parameters
 
     def reset_episode(self):
